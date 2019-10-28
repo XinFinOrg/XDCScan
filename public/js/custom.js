10c4fc145cfec5c82b1d2bf4d7430083905dd923
Dattatray <dattatrayh@gmail.com>
2019-10-28 19:26:21 +0530

    Changes
    Changes done as per Google sheet

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();   
+    $('[data-toggle="tooltip"]').tooltip();
+	
+	$('.collapse').on('shown.bs.collapse', function () {
+		$('#collapsedLink').attr('title', 'Click to see less');
+    	//console.log('show');
+    });
+
+    $('.collapse').on('hidden.bs.collapse', function () {               
+		$('#collapsedLink').attr('title', 'Click to see more');
+    });
 });
