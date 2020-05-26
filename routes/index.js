94eb4d6d5513acae2156dea125d42dc9b6c0406c
AnilChinchawale <anil24593@gmail.com>
2020-05-26 10:50:32 +0530

    resignMNCount update

--- routes/index.js
+++ routes/index.js
@@ -45,7 +45,7 @@ var masterNodeContract;
 var web3relay;
 var contractAddress = "xdc0000000000000000000000000000000000000088";
 var burntAddress = "xdc0000000000000000000000000000000000000000";
-let resignMNCount = 9;
+let resignMNCount = 11;
 let epochRewards = 5000;
 let epochInDay = 48;
 let burntBalance, totalMasterNodesVal, totalStakedValueVal, mnDailyRewards, totalXDC, cmc_xdc_price;
