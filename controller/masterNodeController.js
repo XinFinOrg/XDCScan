360ae5861707ed14580f483a1d729bc2685c4c2d
AnilChinchawale <anil24593@gmail.com>
2020-06-29 17:03:03 +0530

    Update rewards.

--- controller/masterNodeController.js
+++ controller/masterNodeController.js
@@ -10,7 +10,7 @@ var xdc3latestrelay = require('../routes/xdc3LatestRelay');
 var contractAddress = "xdc0000000000000000000000000000000000000088";
 var burntAddress = "xdc0000000000000000000000000000000000000000";
 let resignMNCount = 11;
-let epochRewards = 5000;
+let epochRewards = 4500;
 let epochInDay = 48;
 let burntBalance, totalMasterNodesVal, totalStakedValueVal, mnDailyRewards, totalXDC, cmc_xdc_price;
 const masterNodeHelper = require('../helpers/masterNodeHelper');
