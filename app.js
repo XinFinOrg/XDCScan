d945d8d5556cb4ed7afca6cd8904f658c5b08467
AnilChinchawale <anil24593@gmail.com>
2020-08-29 09:02:30 +0530

    Add Expires headers

--- app.js
+++ app.js
@@ -32,10 +32,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  maxAge: 86400000,
+  setHeaders: function(res, path) {
+      res.setHeader("Expires", new Date(Date.now() + 2592000000*30).toUTCString());
+    }
+}));
 
 // app libraries
 global.__lib = __dirname + '/lib/';
